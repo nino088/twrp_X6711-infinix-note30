41701ad5dbdb645a59195a579657281a5de02628
nino <pilopilosimon1990@gmail.com>
2026-08-22 20:31:07 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -188,24 +188,7 @@ TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 # Version
 TW_DEVICE_VERSION := X6711_NINO
 
-#SHRP-specific lines
-SHRP_PATH := device/infinix/X6711
-SHRP_MAINTAINER := NINO
-SHRP_DEVICE_CODE := X6711
-SHRP_OFFICIAL := true
-SHRP_INTERNAL := /sdcard
-SHRP_EXTERNAL := /sdcard1
-SHRP_OTG := /usb_otg
-SHRP_REC_TYPE := SAR
-SHRP_DEVICE_TYPE := A/B
-SHRP_EXPRESS := true
-SHRP_DARK := true
-SHRP_EXPRESS_USE_DATA := true
-SHRP_FLASH := 1
-SHRP_CUSTOM_FLASHLIGHT := true
-SHRP_FONP_1 := /sys/class/torch/torch/torch_level
-SHRP_FLASH_MAX_BRIGHTNESS := 2
-SHRP_REC := /dev/block/platform/bootdevice/by-name/vendor_boot
-SHRP_NOTCH := true
-SHRP_STATUSBAR_RIGHT_PADDING := 20
-SHRP_STATUSBAR_LEFT_PADDING := 20
+# PBRP specific build flags and enable torch 
+PB_TORCH_PATH := "/sys/class/torch/torch/torch_level"
+PB_TORCH_MAX_BRIGHTNESS := 1
+PB_DISABLE_DEFAULT_TREBLE_COMP := true
